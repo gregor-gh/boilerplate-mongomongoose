bb26f7d34dfbdd913103d2e5c7e79683cc1638e5
gregor-gh <gregor@live.co.uk>
2021-02-12 21:41:51 +0000

    Added removeManyPeople

--- myApp.js
+++ myApp.js
@@ -111,8 +111,11 @@ mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology
     
     const removeManyPeople = (done) => {
       const nameToRemove = "Mary";
-    
-      done(null /*, data*/);
+      
+      Person.remove({name:nameToRemove},(err,data) => {
+        if(err) return console.log(err);
+        done(null,data);
+      });
     };
     
     const queryChain = (done) => {
