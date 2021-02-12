35280514df208dc2de8321a83fdaac2fca91d807
gregor-gh <gregor@live.co.uk>
2021-02-12 19:00:47 +0000

    Added personSchema

--- myApp.js
+++ myApp.js
@@ -1,13 +1,40 @@
-const { Mongoose } = require('mongoose');
+//const { Mongoose } = require('mongoose');
 
 require('dotenv').config();
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology:true})
   .then(() => console.log("MongoDB connected."))
-  .catch(e => console.log(e))
+  .catch(e => console.log(e));
 
-let Person;
+/*
+const db = mongoose.connection;
+
+db.on("error",console.error.bind(console, "connection error:"));
+db.once("open", () => {
+
+  const kittySchema = new mongoose.Schema({
+    name: String
+  });
+
+});
+*/
+
+const personSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    age: Number,
+    favouriteFoods: [String]
+  });
+
+let Person = mongoose.model("Person", personSchema);
+
+/*
+{
+  name: "Gregor",
+  age: 28,
+  favouriteFoods: ["Pizza","Chicken Nuggets","Ice Cream"]
+}
+*/
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
