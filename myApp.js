13155002684ed2ac33e3dbb34c49cf7e162aab18
gregor-gh <gregor@live.co.uk>
2021-02-12 19:45:28 +0000

    personSchema third attempt

--- myApp.js
+++ myApp.js
@@ -4,7 +4,98 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology:true})
-  .then(() => console.log("MongoDB connected."))
+  .then(() => {
+    console.log("MongoDB connected.")
+
+    const Schema = mongoose.Schema
+
+    const personSchema = new Schema({
+        name: {type: String, required: true},
+        age: Number,
+        favouriteFoods: {type: [String], required: false}
+      });
+    
+    let Person = mongoose.model("Person", personSchema);
+    
+    const createAndSavePerson = (done) => {
+      const gregor = new Person({
+        name: "Gregor",
+        age: 28,
+        favouriteFoods: ["Pizza","Chicken Nuggets","Ice Cream"]
+      });
+    
+      gregor.save((err,data) => {
+        if(err) return done(err);
+        done(null,data)
+      });
+    };
+
+
+
+
+    const createManyPeople = (arrayOfPeople, done) => {
+      done(null /*, data*/);
+    };
+    
+    const findPeopleByName = (personName, done) => {
+      done(null /*, data*/);
+    };
+    
+    const findOneByFood = (food, done) => {
+      done(null /*, data*/);
+    };
+    
+    const findPersonById = (personId, done) => {
+      done(null /*, data*/);
+    };
+    
+    const findEditThenSave = (personId, done) => {
+      const foodToAdd = "hamburger";
+    
+      done(null /*, data*/);
+    };
+    
+    const findAndUpdate = (personName, done) => {
+      const ageToSet = 20;
+    
+      done(null /*, data*/);
+    };
+    
+    const removeById = (personId, done) => {
+      done(null /*, data*/);
+    };
+    
+    const removeManyPeople = (done) => {
+      const nameToRemove = "Mary";
+    
+      done(null /*, data*/);
+    };
+    
+    const queryChain = (done) => {
+      const foodToSearch = "burrito";
+    
+      done(null /*, data*/);
+    };
+    
+    /** **Well Done !!**
+    /* You completed these challenges, let's go celebrate !
+     */
+    
+    //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+    
+    exports.PersonModel = Person;
+    exports.createAndSavePerson = createAndSavePerson;
+    exports.findPeopleByName = findPeopleByName;
+    exports.findOneByFood = findOneByFood;
+    exports.findPersonById = findPersonById;
+    exports.findEditThenSave = findEditThenSave;
+    exports.findAndUpdate = findAndUpdate;
+    exports.createManyPeople = createManyPeople;
+    exports.removeById = removeById;
+    exports.removeManyPeople = removeManyPeople;
+    exports.queryChain = queryChain;
+
+  })
   .catch(e => console.log(e));
 
 /*
@@ -20,87 +111,3 @@ db.once("open", () => {
 });
 */
 
-const Schema = mongoose.Schema
-
-const personSchema = new Schema({
-    name: {type: String, required: true},
-    age: Number,
-    favouriteFoods: [String]
-  });
-
-let Person = mongoose.model("Person", personSchema);
-
-const createAndSavePerson = (done) => {
-  const gregor = new Person({
-    name: "Gregor",
-    age: 28,
-    favouriteFoods: ["Pizza","Chicken Nuggets","Ice Cream"]
-  });
-
-  gregor.save((err,data) => {
-    if(err) return done(err);
-    done(null,data)
-  });
-};
-
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
-};
-
-const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
-};
-
-const findOneByFood = (food, done) => {
-  done(null /*, data*/);
-};
-
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
-};
-
-const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
-};
-
-const findAndUpdate = (personName, done) => {
-  const ageToSet = 20;
-
-  done(null /*, data*/);
-};
-
-const removeById = (personId, done) => {
-  done(null /*, data*/);
-};
-
-const removeManyPeople = (done) => {
-  const nameToRemove = "Mary";
-
-  done(null /*, data*/);
-};
-
-const queryChain = (done) => {
-  const foodToSearch = "burrito";
-
-  done(null /*, data*/);
-};
-
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
-
-//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
-
-exports.PersonModel = Person;
-exports.createAndSavePerson = createAndSavePerson;
-exports.findPeopleByName = findPeopleByName;
-exports.findOneByFood = findOneByFood;
-exports.findPersonById = findPersonById;
-exports.findEditThenSave = findEditThenSave;
-exports.findAndUpdate = findAndUpdate;
-exports.createManyPeople = createManyPeople;
-exports.removeById = removeById;
-exports.removeManyPeople = removeManyPeople;
-exports.queryChain = queryChain;
